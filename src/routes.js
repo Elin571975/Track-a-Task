5563c0dd005cb2d83152cb3a8907a401d25cea90
Elin571975 <elin571975@gmail.com>
2019-04-15 20:53:55 -0400

    Apr.15

---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Redirect, Route, Router } from 'react-router-dom';
+import App from './App';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Callback from './components/Callback';
+import Auth from './Auth';
+import history from './history';
+
+const auth = new Auth();
+
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+export const makeMainRoutes = () => {
+  return (
+
+    <Router history={history}>
+    
+        <div>
+          <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+          <Route path="/profile" render={(props) => (
+            !auth.isAuthenticated() ? (
+              <Redirect to="/home"/>
+            ) : (
+              <Profile auth={auth} {...props} />
+            )
+          )} />
+          <Route path="/callback" render={(props) => {
+            handleAuthentication(props);
+            return <Callback {...props} /> 
+          }}/>        
+        </div>
+      </Router>
+  );
+}
